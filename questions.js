57d2bd7a0053397c40b7ec6d3496786438de8d63
Bhati-Akash <akashbhati67@gmail.com>
2024-09-26 23:03:29 +0530

    closures practice

--- questions.js
+++ questions.js
@@ -1,4 +1,5 @@
 // callback function example
+
 function multiply(x) {
     return x*x
 }
@@ -8,6 +9,16 @@ function callbackEx(num, fn) {
     let newNum = fn(num)
     return newNum
 }
-
 let finalNum = callbackEx(a,multiply)
 console.log(finalNum);
+
+
+// for loop and array spreader
+let arr = [1, 2, 3, 4, 5, 6, 7, 8]
+if (true) {
+    for (var i = arr.length - 1; i >= 0; i--) {
+        console.log(arr[i])
+    }
+}
+
+// closures 
